35d8f6b5080f1b0403b62e00ee3ece5ac1a422f6
Ryan Hanson <ryan@codechimps.com>
2015-06-02 23:44:10 -0600

    Determined 2 good keysizes to start decrypting with

--- Basics/CrackRepeatingKey.py
+++ Basics/CrackRepeatingKey.py
@@ -24,6 +24,15 @@ def raw_hamming_distance(bin1, bin2):
 
     return distance
 
+def average_distance(chunks, count):
+    total = 0
+
+    for a in range(0, count, 2):
+        total += raw_hamming_distance(chunks[a], chunks[a+1])
+
+    return float(total) / count
+
+
 test_str1 = "this is a test"
 test_str2 = "wokka wokka!!!"
 correct_distance = 37
@@ -32,6 +41,7 @@ test_distance = hamming_distance(test_str1, test_str2)
 print "[-] Validating Hamming Distance function..."
 print "[-] Finding Distance Between: '" + test_str1 + "' and '" + test_str2 + "'"
 print "[+] Hamming Distance Found: " + str(test_distance)
+
 if test_distance == correct_distance:
     print "[+] Hamming Distance function is working properly!"
 
@@ -57,11 +67,11 @@ with open('6.txt', 'r') as encrypted_file:
         second_chunk = chunk_set[1]
         keysize = len(first_chunk)
         chunk_distance = raw_hamming_distance(first_chunk, second_chunk)
-        normalized_distance = float(chunk_distance) / float(keysize)
+        normalized_distance = (float(chunk_distance) / keysize)
 
         print "[-] Testing chunks with size of: " + str(keysize)
-        print "[-] First chunk: " + str(first_chunk)
-        print "[-] Second chunk: " + str(second_chunk)
+        print "[-] Chunk[0]: " + str(first_chunk)
+        print "[-] Chunk[1]: " + str(second_chunk)
         print "[+] Hamming distance for keysize " + str(keysize) + " is: " + str(chunk_distance)
         print "[+] Normalized distance is: " + str(normalized_distance)
 
@@ -71,6 +81,31 @@ with open('6.txt', 'r') as encrypted_file:
 
     print "[-] The results are in..."
     print "[+] Smallest distance keysize is " + str(best_keysize) + " with distance of: " + str(smallest_distance)
+    print "[-] Lets try average between 4 chunks..."
+
+    smallest_avg_distance = 9999
+    best_avg_keysize = 0
+    for chunk_set in binary_chunks:
+        chunk_count = 4
+        average = average_distance(chunk_set, chunk_count)
+        avg_keysize = len(chunk_set[0])
+        normalized_average = (float(average) / avg_keysize)
+
+        print "[-] Testing chunks with size of: " + str(avg_keysize)
+
+        for x in range(0, chunk_count, 1):
+            print "[+] Chunk["+str(x)+"]: " + str(chunk_set[x])
+
+        print "[+] Average distnace between " + str(chunk_count) + " chunks is: " + str(average)
+        print "[+] Normalized average distance is: " + str(normalized_average)
+
+        if normalized_average < smallest_avg_distance:
+            smallest_avg_distance = normalized_average
+            best_avg_keysize = avg_keysize
+
+    print "[-] Results for averaging 4 chunks..."
+    print "[+] Smallest avg distance keysize " + str(best_avg_keysize) + " with distance of: " + str(smallest_avg_distance)
+    print "[+] Best possible keysizes are: " + str(best_keysize) + " and " + str(best_avg_keysize)
 
     # TODO:
     # Try taking 4 keysize blocks and averaging the distances between them
