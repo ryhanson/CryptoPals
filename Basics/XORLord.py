a486d623a0da281fff8a99cd2b6108a0949afcb4
Ryan Hanson <ryan@codechimps.com>
2015-06-04 10:43:18 -0600

    Updated XORLORD

--- Basics/XORLord.py
+++ Basics/XORLord.py
@@ -1,4 +1,13 @@
 class XORLord:
+    common_words = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with',
+                    'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her',
+                    'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up',
+                    'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time',
+                    'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could',
+                    'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think',
+                    'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new',
+                    'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us']
+
     def __init__(self, plain="", encrypted=""):
         self.plain = plain
         self.encrypted = encrypted
@@ -26,3 +35,14 @@ class XORLord:
         self.plain = decrypted
 
         return self.plain
+
+    def find_words(self, plain):
+        words_found = []
+
+        for y in range(0, len(self.common_words)):
+            word = " " + self.common_words[y] + " "
+
+            if word in plain:
+                words_found.append(self.common_words[y])
+
+        return words_found
